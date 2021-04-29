80aabc1087b90bbf9fb684cda29f708ab1ac7d93
SophiaL1024 <sophiafengliu@gmail.com>
2021-04-29 14:46:28 -0700

    use callback to filter element

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length - 1; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+const takeUntil = function (array, callback) {
+  const results = [];
+  array.forEach(element => {
+    if (!callback(element)) {
+      results.push(element);
+    }
+    else return results;
+  });
+  return results;
+}
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
