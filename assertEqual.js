ba21b0170a6946cccb2552a2ff83b8b385bca9af
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-03 13:03:54 -0700

    refactor assertEqual.js and head.js by using module

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-// TEST CODE
-/* assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1); */
+module.exports=assertEqual;
--- eqObjects.js
+++ eqObjects.js
@@ -21,18 +21,22 @@ const eqArrays = function (arr1, arr2) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
-  } else {
-    for (const key of Object.keys(object1)) {
-      if (object1[key] instanceof Array) {
-        return eqArrays(object1[key], object2[key]);
-      } else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-    return true;
-  }
+ if(Array.isArray(object1) && Array.isArray(object2)){
+   if (Object.keys(object1).length !== Object.keys(object2).length) {
+     return false;
+   } else {
+     for (const key of Object.keys(object1)) {
+       if (object1[key] instanceof Array) {
+         return eqArrays(object1[key], object2[key]);
+       } else if (object1[key] !== object2[key]) {
+         return false;
+       }
+     }
+     return true;
+   }else{
+
+   }
+ }
 };
 /* const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -47,3 +51,6 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false);
