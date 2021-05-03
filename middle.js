803ac5ac2254d109bf0ec9c31fb2ca3d785d6da9
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-03 13:29:34 -0700

    refactor middle.js by using module

--- middle.js
+++ middle.js
@@ -1,22 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length - 1; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-}
 const middle = function(array) {
   const result = [];
   if(array.length===1||array.length===2){
@@ -34,15 +15,4 @@ const middle = function(array) {
     return result;
   }    
 }
-// console.log(eqArrays([1],[1]));
-assertArraysEqual(middle([1]), []) // => []
-assertArraysEqual(middle([1, 2]), []) // => []
-assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
-
-/* console.log(middle([1, 2, 3]), [2]) // => [2]
-console.log(middle([1, 2, 3, 4, 5]), [3]) // => [3]
-console.log(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]), [3, 4])  */
+module.exports=middle;
