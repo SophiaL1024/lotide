cd5abe53b15e3631e444c9d9cf5023714a4d12ff
SophiaL1024 <sophiafengliu@gmail.com>
2021-05-03 21:04:52 +0000

    refactor headTest.js by mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,27 @@
-const head=require('../head')
+/* const head=require('../head')
 const assertEqual=require('../assertEqual')
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
-assertEqual(head([7]), 7);
+assertEqual(head([7]), 7); */
+
+const assert = require('chai').assert;
+const head = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse, 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  })
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+  it("returns 7 for [7]", () => {
+    assert.strictEqual(head([7]), 7);
+  })
+});
